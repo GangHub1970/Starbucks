bf3fd98a87bd891fa12164c9e84d5f55bd3804c1
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2022-10-24 15:27:14 +0900

    HEADER-badges

--- js/main.js
+++ js/main.js
@@ -12,3 +12,16 @@ input.addEventListener("blur", () => {
   input.placeholder = "";
   searchIcon.classList.remove("hidden");
 });
+
+const badges = document.querySelector(".badges");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 500) {
+    badges.style.opacity = 0;
+    badges.style.pointerEvents = "none";
+  } else {
+    badges.style.opacity = 1;
+    badges.classList.remove("hidden");
+    badges.style.pointerEvents = "auto";
+  }
+});
