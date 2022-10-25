3bd5986d80372f5c97bf311ca3c46aefb2da6bb0
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2022-10-25 17:20:41 +0900

    애니메이션 추가

--- js/main.js
+++ js/main.js
@@ -14,16 +14,24 @@ input.addEventListener("blur", () => {
 });
 
 const badges = document.querySelector(".badges");
+const toTopBtn = document.querySelector(".to-top");
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 500) {
     badges.style.opacity = 0;
     badges.style.pointerEvents = "none";
+    toTopBtn.style.opacity = 1;
+    toTopBtn.style.pointerEvents = "auto";
   } else {
     badges.style.opacity = 1;
-    badges.classList.remove("hidden");
     badges.style.pointerEvents = "auto";
+    toTopBtn.style.opacity = 0;
+    toTopBtn.style.pointerEvents = "none";
   }
+});
+
+toTopBtn.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
 
 const noticeSwiper = new Swiper(".notice-line .swiper", {
@@ -38,6 +46,7 @@ const noticeSwiper = new Swiper(".notice-line .swiper", {
 const promotionSwiper = new Swiper(".promotion .swiper", {
   autoplay: {
     delay: 5000,
+    disableOnInteraction: false,
   },
   loop: true,
   slidesPerView: 3,
@@ -55,7 +64,8 @@ const promotionSwiper = new Swiper(".promotion .swiper", {
 
 const awardSwiper = new Swiper(".awards .swiper", {
   autoplay: {
-    delay: 5000,
+    delay: 3000,
+    disableOnInteraction: false,
   },
   loop: true,
   slidesPerView: 5,
@@ -83,4 +93,36 @@ promotionBtn.addEventListener("click", () => {
   }
 
   promotionIsHide = !promotionIsHide;
+});
+
+const visualEls = document.querySelectorAll(".fade-in");
+visualEls.forEach((visualEl, index) => {
+  gsap.to(visualEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
+  });
+});
+
+function random(min, max) {
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+
+const floatingEls = document.querySelectorAll(".floating");
+floatingEls.forEach((floatingEl) => {
+  gsap.to(floatingEl, random(1.5, 2.5), {
+    y: random(15, 20),
+    repeat: -1,
+    yoyo: true,
+    ease: Power1.easeInOut,
+  });
+});
+
+const spyEls = document.querySelectorAll("section.scroll-spy");
+spyEls.forEach((spyEl) => {
+  new ScrollMagic.Scene({
+    triggerElement: spyEl,
+    triggerHook: 0.8,
+  })
+    .setClassToggle(spyEl, "show")
+    .addTo(new ScrollMagic.Controller());
 });
