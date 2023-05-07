eb1d8e2613a9ef53aaafb6b40f7b4645526749aa
GangHub1970 <dbsrkd1970@pusan.ac.kr>
2023-05-07 22:04:49 +0900

    feat: Create Badges
    - Lodash, GSAP 라이브러리 추가
    - 뱃지를 추가하고, 라이브러리로 스크롤 위치에 따른 뱃지 애니메이션 추가

--- js/main.js
+++ js/main.js
@@ -14,3 +14,25 @@ searchInputEl.addEventListener("blur", () => {
   searchEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+const badgeEl = document.querySelector("header .badges");
+
+// _.throttle(함수, 시간(ms))
+// lodash의 throttle을 이용해서 scroll 이벤트가 짧은 시간에 너무 많이 발생하지 않게 조절한다.
+window.addEventListener(
+  "scroll",
+  _.throttle(() => {
+    if (window.scrollY > 500) {
+      // gsap.to(요소, 지속시간, 옵션);
+      gsap.to(badgeEl, 0.6, {
+        opacity: 0,
+        display: "none",
+      });
+    } else {
+      gsap.to(badgeEl, 0.6, {
+        opacity: 1,
+        display: "block",
+      });
+    }
+  }, 300)
+);
